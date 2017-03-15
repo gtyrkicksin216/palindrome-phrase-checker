7f98f682fd529aa77093bf75ad92d2d9e4490605
Ryan Wood <r0178658@gmail.com>
2017-03-15 13:50:50 -0400

    First commit to project

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// SET THE VARIABLES TO GET DATA FROM AN INPUT
+
+const bodyVerify = document.getElementsByClassName('container');
+var forward = [];
+var reverse = [];
+
+document.getElementById('submit').onclick = function (){
+    // GET THE USER INPUT AND STRIP IT OF SPACES AND COMMAS
+    // USE THAT TO SET THE FORWARD VAR
+    forward = ((document.getElementById('user-input').value).replace(/\s/g, '').replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, '')).toLowerCase();
+    // TAKE FORWARD VAR AND REVERSE IT TO SET THE REVERSE VAR
+    reverse = forward.split('').reverse().join('');
+    // THEN COMPARE THE TWO
+    if (forward === reverse){
+        // DO SOMETHING IF IT'S A PALENDROME
+        console.log('palindrome!!');
+    } else {
+        // DO SOMETHING ELSE IF IT IS NOT A PALENDROME
+        console.log('nope!');
+    }
+};
